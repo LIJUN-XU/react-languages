35ff58fd9398e4363d6b67ae5588f88952ef3dc0
徐丽君 <xulj19@vanke.com>
2024-01-31 16:41:20 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import { useEffect,useState } from 'react'
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import browserLang from 'browser-lang';
-import {IntlProvider, FormattedMessage} from 'react-intl'
-const supportedLanguages = ["en","zh","es","ar"];
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const defaultLocale = browserLang({
-    languages: supportedLanguages,
-    fallback: 'en'
-  });
-  const [locale, setLocale] = useState(defaultLocale);
-  const [messages, setMessages] = useState(undefined);
-  useEffect(() => {
-    const apiKey = 'Ydcivo7woXSvaw0fhE6NpQ';
 
-    fetch(
-      `https://api.i18nexus.com/project_resources/translations/${locale}/default?api_key=${apiKey}`
-    )
-      .then(response => response.json())
-      .then(data => {
-        setMessages(data);
-      });
-  }, [locale]);
 
   return (
-    <IntlProvider locale={locale} messages={messages}>
+    <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -38,8 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <p>
-        <FormattedMessage id="welcome_msg" />
-        {/* welcome to my app! */}
+        welcome to my app!
       </p>
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -52,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-    </IntlProvider>
+    </>
   )
 }
 
